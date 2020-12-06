5836ad8619cccef61a4d3f98b7374a521e1b6b50
Suh Donghwi <hwidongsuh@gmail.com>
2020-12-07 05:05:53 +0900

    feat: add transition animation

--- src/components/AtomPillar.tsx
+++ src/components/AtomPillar.tsx
@@ -1,6 +1,9 @@
-import { useRef, useEffect } from "react";
+import { useRef, useEffect, useState } from "react";
+import { useFrame } from "react-three-fiber";
+import usePrevious from "../hooks/usePrevious";
 import { Mesh, BoxBufferGeometry } from "three";
 import { Text } from "@react-three/drei";
+import { animated, useSpring } from "@react-spring/three";
 
 import AtomInfo from "../types/AtomInfo";
 
@@ -23,40 +26,52 @@ export default function AtomPillar({
   const symbolText = useRef<Text>();
   const numberText = useRef<Text>();
 
+  const meshProps = useSpring({ scale: [1, height, 1] as any });
+
+  const textProps = useSpring({
+    position: [position[0], position[1] + height, position[2]] as any,
+  });
+
   useEffect(() => {
-    mesh.current?.position.setY(height / 2);
+    geometry.current?.translate(0, 0.5, 0);
 
     // @ts-ignore
-    symbolText.current?.position.setY(height);
+    //symbolText.current?.position.setY(height);
     // @ts-ignore
-    numberText.current?.position.setY(height);
-  }, [height]);
+    //numberText.current?.position.setY(height);
+  }, []);
 
   return (
     <group>
-      <mesh position={[position[0], position[1], position[2]]} ref={mesh}>
-        <boxBufferGeometry ref={geometry} args={[1, height, 1]} />
+      <animated.mesh
+        ref={mesh}
+        position={[position[0], position[1], position[2]]}
+        {...meshProps}
+      >
+        <boxBufferGeometry ref={geometry} args={[1, 1, 1]} />
         <meshStandardMaterial color={color} />
-      </mesh>
-      <Text
-        ref={symbolText}
-        position={[position[0], position[1], position[2] + 0.1]}
-        rotation={[-Math.PI / 2, 0, 0]}
-        fontSize={0.5}
-        depthOffset={-0.1}
-      >
-        {atom.symbol}
-      </Text>
+      </animated.mesh>
+      <animated.group {...textProps}>
+        <Text
+          ref={symbolText}
+          position={[0, 0, 0.1]}
+          rotation={[-Math.PI / 2, 0, 0]}
+          fontSize={0.5}
+          depthOffset={-0.1}
+        >
+          {atom.symbol}
+        </Text>
 
-      <Text
-        ref={numberText}
-        position={[position[0], position[1], position[2] - 0.3]}
-        rotation={[-Math.PI / 2, 0, 0]}
-        fontSize={0.2}
-        depthOffset={-0.1}
-      >
-        {atom.number.toString()}
-      </Text>
+        <Text
+          ref={numberText}
+          position={[0, 0, -0.25]}
+          rotation={[-Math.PI / 2, 0, 0]}
+          fontSize={0.2}
+          depthOffset={-0.1}
+        >
+          {atom.number.toString()}
+        </Text>
+      </animated.group>
     </group>
   );
 }

bc4322472c2623a510c7a2690c236a174e464ffc
Suh Donghwi <hwidongsuh@gmail.com>
2021-02-15 21:01:18 +0900

    chore: make atom pillar accept MeshProps

--- src/components/AtomPillar.tsx
+++ src/components/AtomPillar.tsx
@@ -4,10 +4,10 @@ import { Text } from "@react-three/drei";
 import { animated, useSpring } from "@react-spring/three";
 
 import AtomInfo from "../types/AtomInfo";
+import { MeshProps } from "react-three-fiber";
 
 interface AtomPillarProps {
   atom: AtomInfo;
-  position: [number, number, number];
   height: number;
   color: string;
 
@@ -16,11 +16,11 @@ interface AtomPillarProps {
 
 export default function AtomPillar({
   atom,
-  position,
   height,
   color,
   onClick,
-}: AtomPillarProps) {
+  ...props
+}: AtomPillarProps & MeshProps) {
   const [hover, setHover] = useState(false);
 
   const mesh = useRef<Mesh>();
@@ -33,9 +33,6 @@ export default function AtomPillar({
     scale: [1, Math.max(height, 0.00001), 1] as any,
   });
 
-  const textProps = useSpring({
-    position: [position[0], position[1] + height, position[2]] as any,
-  });
   const colorProps = useSpring({
     to: {
       color: color,
@@ -50,30 +47,29 @@ export default function AtomPillar({
   }, []);
 
   return (
-    <group>
-      <animated.mesh
-        ref={mesh}
-        position={[position[0], position[1], position[2]]}
-        onClick={(e) => {
-          e.stopPropagation();
-          onClick(atom);
-        }}
-        onPointerOver={(e) => {
-          e.stopPropagation();
-          setHover(true);
-        }}
-        onPointerOut={(e) => {
-          e.stopPropagation();
-          setHover(false);
-        }}
-        {...meshProps}
-      >
-        <boxBufferGeometry ref={geometry} args={[1, 1, 1]} />
-        <animated.meshStandardMaterial
-          color={hover ? "#ff8787" : colorProps.color}
-        />
-      </animated.mesh>
-      <animated.group {...textProps}>
+    <animated.mesh
+      ref={mesh}
+      onClick={(e) => {
+        e.stopPropagation();
+        onClick(atom);
+      }}
+      onPointerOver={(e) => {
+        e.stopPropagation();
+        setHover(true);
+      }}
+      onPointerOut={(e) => {
+        e.stopPropagation();
+        setHover(false);
+      }}
+      {...meshProps}
+      {...props}
+    >
+      <boxBufferGeometry ref={geometry} args={[1, 1, 1]} />
+      <animated.meshStandardMaterial
+        color={hover ? "#ff8787" : colorProps.color}
+      />
+
+      <animated.group position={[0, 1, 0]}>
         <Text
           ref={symbolText}
           position={[0, 0, 0.05]}
@@ -94,6 +90,6 @@ export default function AtomPillar({
           {atom.number.toString()}
         </Text>
       </animated.group>
-    </group>
+    </animated.mesh>
   );
 }
